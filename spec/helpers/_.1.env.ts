ae3210276265716320e4ee38f826545503711f64
J.P. Poveda <jp@poveda.net>
2017-08-13 16:43:27 +0530

    #18

--- spec/helpers/_.1.env.ts
+++ spec/helpers/_.1.env.ts
@@ -2,7 +2,7 @@ import 'colors';
 
 process.env.NODE_ENV = 'local-test';
 process.env.TEST_MONGO_DB_ADDRESS = process.env.TEST_MONGO_DB_ADDRESS || 'localhost';
-process.env.TEST_MONGO_DB_PORT = process.env.TEST_MONGO_DB_PORT || 37001;
+process.env.TEST_MONGO_DB_PORT = process.env.TEST_MONGO_DB_PORT || '37001';
 
 // tslint:disable-next-line:no-console
 console.log((`NODE_ENV set to ${process.env.NODE_ENV}`.green));
